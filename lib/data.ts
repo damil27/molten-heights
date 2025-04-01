d4e8646e4f5cf04a012463116eb333e4ec430ac6
damin <34189426+damil27@users.noreply.github.com>
2025-04-01 12:04:47 +0100

    create layout completed

---
+++ lib/data.ts
@@ -0,0 +1,38 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Home",
+      href: "/search?tag=todays-deal",
+    },
+    {
+      name: "Product & Brand ",
+      href: "/search?tag=new-arrival",
+    },
+    {
+      name: "Featured Products",
+      href: "/search?tag=featured",
+    },
+    {
+      name: "Pick & Mix",
+      href: "/search?tag=best-seller",
+    },
+    {
+      name: "Gift Ideas",
+      href: "/#browsing-history",
+    },
+    {
+      name: "Customer Service",
+      href: "/page/customer-service",
+    },
+    {
+      name: "About Us",
+      href: "/page/about-us",
+    },
+    {
+      name: "Help",
+      href: "/page/help",
+    },
+  ],
+};
+
+export default data;
