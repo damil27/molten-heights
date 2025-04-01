f9a5f38d134e07c1b938e2e126178c00b5b12fbe
damin <34189426+damil27@users.noreply.github.com>
2025-04-01 11:07:50 +0100

    initial commit massage for project setuptools

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "MoltenHeight";
+
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An Ecommanece site with Next.js and MongoDB  ";
